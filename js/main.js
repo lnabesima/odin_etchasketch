16deb618dc992caf8070c410727724503ef19a49
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-02-13 17:48:39 -0300

    Add painting functionality on mouse enter

--- js/main.js
+++ js/main.js
@@ -3,9 +3,15 @@ const mainDiv = document.querySelector('.div-wrapper');
 const createDiv = () => {
   const newDiv = document.createElement('div');
   newDiv.style.cssText = 'width: 20px; height: 20px; border: 1px solid black';
+  newDiv.classList.add('div-wrapper__block');
   mainDiv.appendChild(newDiv);
 };
 
 for (let i = 0; i < 256; i += 1) {
   createDiv();
 }
+
+const basicBlocks = document.querySelectorAll('.div-wrapper__block');
+[...basicBlocks].forEach((block) => {
+  block.addEventListener('mouseenter', () => block.classList.add('test'));
+});
